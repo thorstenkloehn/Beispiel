ed49818047a7a8c5d381cca485407b1fe2fc2fdb
Thorsten Klöhn <thorstenkloehn@gmail.com>
2023-05-08 00:14:45 +0200

    start

---
+++ 02_Variable/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+    int zahl=43;
+    std::cout << zahl << std::endl;
+    return 0;
+}
---
+++ 02_Variable/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let zahl = 43;
+    println!("{}", zahl);
+}
